9bc5568ddc2f77382a7c6e2fbbc610b6f2382405
Cecylia Bocovich <cohosh@torproject.org>
2019-05-31 16:03:01 -0400

    Partial implementation of a sequencing layer
    This is a first stab at a sequencing layer for snowflake. For now there
    are very simple headers with only sequence and acknowledgement numbers

---
+++ common/snowflake-proto/proto.go
@@ -0,0 +1,126 @@
+package proto
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+)
+
+// Fixed size window used for sequencing and reliability layer
+const windowSize = 1500
+const snowflakeHeaderLen = 12
+
+type snowflakeHeader struct {
+	seq    int
+	ack    int
+	length int //length of the accompanying data (not including header length)
+}
+
+func (h *snowflakeHeader) Parse(b []byte) error {
+	h.seq = int(binary.LittleEndian.Uint32(b[0:4]))
+	h.ack = int(binary.LittleEndian.Uint32(b[4:8]))
+	h.length = int(binary.LittleEndian.Uint32(b[8:12]))
+
+	return nil
+}
+
+// Converts a header to bytes
+func (h *snowflakeHeader) Marshal() ([]byte, error) {
+	if h == nil {
+		return nil, fmt.Errorf("nil header")
+	}
+	b := make([]byte, 12, 12)
+	binary.LittleEndian.PutUint32(b[0:4], uint32(h.seq))
+	binary.LittleEndian.PutUint32(b[4:8], uint32(h.ack))
+	binary.LittleEndian.PutUint32(b[8:12], uint32(h.length))
+
+	return b, nil
+
+}
+
+// Parses a webRTC header from bytes received on the
+// webRTC connection
+func ParseHeader(b []byte) (*snowflakeHeader, error) {
+
+	h := new(snowflakeHeader)
+	if err := h.Parse(b); err != nil {
+		return nil, err
+	}
+	return h, nil
+}
+
+type SnowflakeReadWriter struct {
+	seq int
+	ack int
+
+	Conn io.ReadWriteCloser
+
+	buffer    []byte
+	remaining int
+	out       []byte
+}
+
+func (s *SnowflakeReadWriter) Read(b []byte) (int, error) {
+	length, err := s.Conn.Read(b)
+	if err != nil {
+		return length, err
+	}
+	s.buffer = append(s.buffer, b...)
+
+	n := copy(b, s.out)
+	s.out = s.out[n:]
+	if n == len(b) {
+		return n, err
+	}
+
+	for len(s.buffer) > 0 {
+		if len(s.buffer) < snowflakeHeaderLen {
+			//we don't have enough data for a full header yet
+			return n, err
+		}
+
+		// first read in the header and update the sequence and acknowledgement numbers
+		header, err := ParseHeader(s.buffer)
+		if err != nil {
+			return n, err
+		}
+		if len(s.buffer) < header.length {
+			//we don't have a full chunk yet
+			return n, err
+		}
+		//for now, drop all data with an incorrect sequence number
+		if header.seq == s.ack {
+
+			s.out = append(s.out, s.buffer[snowflakeHeaderLen:header.length]...)
+			s.ack += len(s.out)
+			//TODO: write an empty length header acknowledging data
+		}
+		s.buffer = s.buffer[header.length:]
+
+		n += copy(b[n:], s.out)
+		s.out = s.out[n:]
+	}
+	return n, err
+}
+
+func (c *SnowflakeReadWriter) Write(b []byte) (int, error) {
+
+	//need to append a header onto
+	h := new(snowflakeHeader)
+	h.length = len(b) + snowflakeHeaderLen
+	h.seq = c.seq
+	h.ack = c.ack
+
+	bytes, err := h.Marshal()
+	if err != nil {
+		return 0, err
+	}
+	bytes = append(bytes, b...)
+	c.seq += h.length - snowflakeHeaderLen
+
+	return c.Conn.Write(bytes)
+}
+
+func (c *SnowflakeReadWriter) Close() error {
+	return c.Conn.Close()
+}
---
+++ common/snowflake-proto/proto_test.go
@@ -0,0 +1,118 @@
+package proto
+
+import (
+	"bytes"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+type stubConn struct {
+	Buf *bytes.Buffer
+}
+
+func (c *stubConn) Read(b []byte) (int, error) {
+	return c.Buf.Read(b)
+}
+
+func (c *stubConn) Write(b []byte) (int, error) {
+	return c.Buf.Write(b)
+}
+
+func (c *stubConn) Close() error {
+	return nil
+}
+
+func TestSnowflakeProto(t *testing.T) {
+	Convey("Connection set up", t, func() {
+
+		buffer := new(bytes.Buffer)
+		sConn := &stubConn{Buf: buffer}
+
+		s := &SnowflakeReadWriter{Conn: sConn}
+
+		Convey("Create correct headers", func() {
+			var sent, received, wire []byte
+			sent = []byte{'H', 'E', 'L', 'L', 'O'}
+			wire = []byte{
+				0x00, 0x00, 0x00, 0x00, //seq
+				0x00, 0x00, 0x00, 0x00, //ack
+				0x11, 0x00, 0x00, 0x00, //len
+				'H', 'E', 'L', 'L', 'O',
+			}
+			received = make([]byte, len(wire), len(wire))
+
+			n, err := s.Write(sent)
+
+			So(n, ShouldEqual, len(sent)+snowflakeHeaderLen)
+			So(err, ShouldEqual, nil)
+			So(sConn.Buf.Bytes(), ShouldResemble, wire)
+
+			n, err = s.Read(received)
+
+			So(err, ShouldEqual, nil)
+			So(n, ShouldEqual, len(sent))
+			So(received[:n], ShouldResemble, sent)
+
+			//Make sure seq and ack have been updated
+			So(s.seq, ShouldEqual, 5)
+			So(s.ack, ShouldEqual, 5)
+
+		})
+
+		Convey("Partial reads work correctly", func() {
+			var sent, received []byte
+			sent = []byte{'H', 'E', 'L', 'L', 'O'}
+			received = make([]byte, 7, 7)
+
+			n, err := s.Write(sent)
+
+			//Read in partial header
+			n, err = s.Read(received)
+
+			So(err, ShouldEqual, nil)
+			So(n, ShouldEqual, 0)
+			So(s.ack, ShouldEqual, 0)
+
+			//Read in first part of message
+			n, err = s.Read(received)
+
+			So(err, ShouldEqual, nil)
+			So(n, ShouldEqual, 0)
+			So(s.ack, ShouldEqual, 0)
+
+			//Read in rest of message
+			n, err = s.Read(received)
+
+			So(err, ShouldEqual, nil)
+			So(n, ShouldEqual, 5)
+			So(s.ack, ShouldEqual, 5)
+			So(received[:n], ShouldResemble, sent)
+		})
+
+		Convey("Test reading multiple chunks", func() {
+			var sent, received []byte
+			sent = []byte{'H', 'E', 'L', 'L', 'O'}
+			received = make([]byte, 15, 15)
+
+			n, err := s.Write(sent)
+			n, err = s.Write(sent)
+			n, err = s.Write(sent)
+
+			for i := 0; i < 4; i++ {
+				n, err = s.Read(received)
+
+				So(err, ShouldEqual, nil)
+				if i == 0 {
+					So(n, ShouldEqual, 0)
+				} else {
+					So(n, ShouldEqual, 5)
+					So(received[:n], ShouldResemble, sent)
+				}
+				So(s.ack, ShouldEqual, i*5)
+			}
+
+		})
+	})
+
+}
